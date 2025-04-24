8c3f0289ceb288fc27b363e7bff9fa1bbe1324d9
Andri Titkov <andrii.titkov@gmail.com>
2025-04-24 15:19:20 +0300

    fix: remove circular import in keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,7 @@
-# keep_alive.py
-
-from keep_alive import keep_alive
-keep_alive()
-
-
 from flask import Flask
 from threading import Thread
 import os
+
 app = Flask('')
 
 @app.route('/')
@@ -16,8 +11,6 @@ def home():
 def run():
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
-
-
 
 def keep_alive():
     t = Thread(target=run)
